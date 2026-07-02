04493ece436459054f65967030a92b478aeb32e1
Torstein Kristoffer Walle Thomassen <tkth@equinor.com>
2026-07-02 13:59:52 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Data
+import pandas as pd
+import numpy as np
+
+# Dato og tid
+from datetime import datetime, timedelta
+
+# Spark / Databricks
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
+from pyspark.sql.window import Window
+
+# Visualisering
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Filbaner
+from pathlib import Path
+
+# Logging
+import logging
+
+def main():
+    print("Hello from myproject!")
+
+
+if __name__ == "__main__":
+    main()
